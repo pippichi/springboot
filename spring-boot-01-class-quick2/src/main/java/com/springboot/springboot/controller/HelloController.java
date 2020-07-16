50fc0b704ec6a4505fb3cb83a823b00ce25ac08b
qyf <emoqyf@163.com>
2020-07-16 16:32:22 +0800

    go

--- spring-boot-01-class-quick2/src/main/java/com/springboot/springboot/controller/HelloController.java
+++ spring-boot-01-class-quick2/src/main/java/com/springboot/springboot/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @Value("${person.last-name}")
-    private  String name;
+    private String name;
 
     @RequestMapping("/sayhello")
     public String sayHello(){
